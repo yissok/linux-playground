3add312785f270212c4d71124a8b7b1ad1a05213
yissok <andreatv12@gmail.com>
2024-05-22 14:43:18 +0100

    open project

---
+++ desktopShortcuts/openNodeProjInVim.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+kgx
+sleep .1s
+#COMMANDS HERE
+
+xdotool type 'cd Documents/GIT/node-bastion'
+xdotool key enter
+xdotool type 'nvim .'
+xdotool key enter
+xdotool key enter
+kgx
+sleep .1s
+xdotool type 'cd Documents/GIT/node-bastion'
+xdotool key enter
+
+#COMMANDS HERE
+sleep .1s
+xdotool key Super+KP_9
+sleep .1s
+xdotool key alt+quoteleft
+sleep .1s
+xdotool key Super+KP_7
+sleep .1s
+xdotool key Escape
+
+xdotool keydown --delay 50  alt
+xdotool key --delay 50 quoteleft quoteleft
+xdotool keyup --delay 50  alt
+sleep .3s
+xdotool key --delay 50 alt+space Up
+xdotool key enter
+
